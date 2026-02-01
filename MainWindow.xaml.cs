4dbd3da3eb2c87a5213a1da3392bbcfd0380b6d2
233box <1634369780@qq.com>
2026-02-02 01:28:51 +0800

    系统边框优化，新增设置标签页

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using CommunityToolkit.Mvvm.DependencyInjection;
 using Sai2Capture.ViewModels;
 using Wpf.Ui.Controls; // 包含 FluentWindow, Button, Card 等
@@ -47,6 +48,50 @@ namespace Sai2Capture
             {
                 _isClosing = false;
             }
+        }
+
+        // 标题栏拖动
+        private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                // 双击最大化/还原
+                MaximizeButton_Click(sender, e);
+            }
+            else
+            {
+                // 单击拖动
+                DragMove();
+            }
+        }
+
+        // 最小化按钮
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        // 最大化/还原按钮
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                WindowState = WindowState.Normal;
+                // 还原图标：单个方框
+                MaximizeIcon.Data = System.Windows.Media.Geometry.Parse("M 0,0 H 10 V 10 H 0 Z");
+            }
+            else
+            {
+                WindowState = WindowState.Maximized;
+                // 最大化图标：双层方框
+                MaximizeIcon.Data = System.Windows.Media.Geometry.Parse("M 0,2 H 8 V 10 H 0 Z M 2,0 H 10 V 8");
+            }
+        }
+
+        // 关闭按钮
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
